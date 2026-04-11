0a26aedb44921fbd360f4a42da740cef0adfe960
Ian Dullu <dr.dullu@outlook.com>
2026-04-11 13:25:44 +0300

    fix: add project name to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Cloudflare Pages Deploy Script
 echo "Build complete. Deploying to Cloudflare Pages..."
-npx wrangler pages deploy .next --project-name=drd-digital --branch=master
+# Use CF_PAGES_PROJECT_NAME if set (Cloudflare provides this), otherwise use default
+PROJECT_NAME="${CF_PAGES_PROJECT_NAME:-drd-digital}"
+npx wrangler pages deploy .next --project-name="$PROJECT_NAME" --branch=master --commit-hash="$CF_PAGES_COMMIT_SHA"
