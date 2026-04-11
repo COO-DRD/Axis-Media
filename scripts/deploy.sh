58475d8e96e7d7ba49c76221ed4c2006d8715c3c
Ian Dullu <dr.dullu@outlook.com>
2026-04-11 13:35:29 +0300

    fix: use correct project name axismedia

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Cloudflare Pages Deploy Script
 echo "Build complete. Deploying to Cloudflare Pages..."
-# Use CF_PAGES_PROJECT_NAME if set (Cloudflare provides this), otherwise use default
-PROJECT_NAME="${CF_PAGES_PROJECT_NAME:-drd-digital}"
-npx wrangler pages deploy .next --project-name="$PROJECT_NAME" --branch=master --commit-hash="$CF_PAGES_COMMIT_SHA"
+npx wrangler pages deploy .next --project-name=axismedia --branch=master
